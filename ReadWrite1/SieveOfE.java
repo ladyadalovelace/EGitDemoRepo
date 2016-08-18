fc932ff4f9c7b63df7d1d78b1bb654c1c16f5897
melodylam <melodylam@melodys-mbp.ad.hisd.org>
2016-08-18 13:57:25 -0500

    Initial Commit
    Testing EGit Within Eclipse

---
+++ ReadWrite1/SieveOfE.java
@@ -0,0 +1,32 @@
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SieveOfE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Set<Integer> nums = new TreeSet<Integer>();
+		int n = 200;
+		
+		for(int i = 2; i < n; i++)
+		{
+			nums.add(i);
+		}
+		
+		for(int i = 2; i < Math.sqrt(n); i++)
+		{
+			Iterator<Integer> iter = nums.iterator(); // use iterator here because can't use enhanced for to change Set 
+			while(iter.hasNext())
+			{
+				Integer x = iter.next();
+				if(x%i == 0 && x!=i) iter.remove();
+			}
+		}
+		
+		System.out.println(nums);
+	}
+	
+
+}
