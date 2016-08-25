8e2566bfa5a8af9baa474df5a99ca3bf19be2550
melodylam <melodylam@melodys-mbp.ad.hisd.org>
2016-08-25 09:35:51 -0500

    # WARNING: head commit changed in the meantime
    Initial Commit
    
    Testing EGit Within Eclipse

--- ReadWrite1/Total.java
+++ ReadWrite1/Total.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Total {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub 
 		Scanner console = new Scanner(System.in);
 		System.out.print("Input file: ");
 		String inputFileName = console.next();
